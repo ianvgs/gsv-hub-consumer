d668795fa7495a3cce72decd04c4c05622501502
Ian <ivg@bb.com.br>
2024-04-05 22:17:32 -0300

    ref: alterando uri chumbada por dotenv

--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,22 @@
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.connectMicroservice<MicroserviceOptions>({
     transport: Transport.RMQ,
     options: {
-      urls: ['amqp://guest:guest@localhost:5672'],
+      urls: [process.env.RABBIT_MQ_URI],
       queue: 'generate_gea_protocol',
       noAck: false,
       persistent: true,
     },
   });
+
   await app.startAllMicroservices();
 }
 bootstrap();
